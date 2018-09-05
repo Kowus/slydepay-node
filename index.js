c7d6d32dad4b98a77f96474ebb25edbf428e5bf2
Kowus <barnabas@elite-education.org>
2018-09-05 15:51:47 +0000

    remove bluebird

--- index.js
+++ index.js
@@ -4,19 +4,49 @@
  * @soundtrack Flowers - Manny Keys
 */
 const invoice = require('./lib/invoicing'),
-    transactions = require('./lib/transaction')
-;
+  transactions = require('./lib/transaction');
+const request = require('request');
 
-module.exports = Merchant;
-
-function Merchant(merchant) {
+class Merchant {
+  constructor(merchant) {
     this.emailOrMobileNumber = merchant.emailOrMobileNumber;
     this.merchantKey = merchant.merchantKey;
+  }
+
+  listPayOptions() {
+    const merchantKey = this.merchantKey,
+      emailOrMobileNumber = this.emailOrMobileNumber;
+    return new Promise((resolve, reject) => {
+      request(
+        {
+          method: 'POST',
+          json: {
+            emailOrMobileNumber: emailOrMobileNumber,
+            merchantKey: merchantKey
+          },
+          url: 'https://app.slydepay.com.gh/api/merchant/invoice/payoptions'
+        },
+        (err, response, body) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          if (!body.success || body.errorCode) {
+            reject({ code: body.errorCode, message: body.errorMessage });
+            return;
+          }
+          resolve(body);
+        }
+      );
+    });
+  }
 }
 
-Merchant.prototype.listPayOptions = invoice.listPayOptions;
+// Merchant.prototype.listPayOptions = invoice.listPayOptions;
 Merchant.prototype.createInvoice = invoice.createInvoice;
 Merchant.prototype.sendInvoice = invoice.sendInvoice;
 Merchant.prototype.checkPaymentStatus = invoice.checkPaymentStatus;
 Merchant.prototype.confirmTransaction = transactions.confirmTransaction;
 Merchant.prototype.cancelTransaction = transactions.cancelTransaction;
+
+module.exports = Merchant;
